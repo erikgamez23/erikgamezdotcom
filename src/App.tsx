731a58a2670504ce443df4afc2bac032560cc3f0
erikgamez23 <erikgamez23@gmail.com>
2025-12-17 23:23:55 -0600

    Add Profile Photo

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,31 @@
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+import erikPhoto from '/erik.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return <MantineProvider>{
     (
       <>
         <div>
-          <a href="https://vite.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
+          {/* <a href="https://vite.dev" target="_blank"> */}
+            <img src={erikPhoto} className="erikPhoto" alt="Erik Gamez" />
+          {/* </a> */}
         </div>
-        <h1>Erik Gamez</h1>
-        <div className="card">
+        <h1> Hey, thanks for visiting! </h1>
+        <h2> Not sure how often I'll update, but I'll try and keep it fresh. </h2>
+        
+        {/* <div className="card">
           <button onClick={() => setCount((count) => count + 1)}>
             {count}
           </button>
-        </div>
+        </div> */}
+        
       </>
     )
   }</MantineProvider>
