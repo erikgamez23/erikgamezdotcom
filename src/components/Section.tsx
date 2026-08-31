6ce0e9442f9133ec224437f31f4e0f9cdef987df
erikgamez23 <erikgamez23@gmail.com>
2026-08-30 21:07:00 -0500

    Fix flexDirection String error type
    Also updates first Section body string

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -18,7 +18,7 @@ const Section: React.FC<SectionProps> = ({
 }) => {
   const hasImage = !!imageSrc;
   const containerStyle = {
-    flexDirection: side === 'left' ? 'row' : 'row-reverse',
+    flexDirection: side === 'left' ? 'row' as 'row' : 'row-reverse' as 'row-reverse',
     ...(hasImage ? {} : { border: '2pt solid var(--color-erikBlue)' }),
     ...(hasImage ? {} : { padding: '24pt' }),
   };
