d0179378a986fd750dfffc23f09484dfff8bcb56
erikgamez23 <erikgamez23@gmail.com>
2026-09-06 23:28:46 -0500

    New design content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,22 @@
-import erikPhoto from '../assets/erik.jpg'
+import leadPhoto from '../assets/erik.jpg'
 import butterfly from '../assets/butterfly.jpeg'
-import Section from '../components/Section'
 import erikMnt from '../assets/erikmountain.jpg'
+// import flower from '../assets/pinkflower.jpeg'
+import Section from '../components/Section'
+
 import '../App.css'
 
 const Home = () => {
     return (
         <>
         <div>
-            <img src={erikMnt} className="erikPhoto" alt="Erik Gamez" />
+            <img src={erikMnt} className="leadPhoto" alt="Erik Gamez" />
         </div>
-        <h1> Hey, I'm Erik! Get to know me a lil bit </h1>
-        <Section title="Who am I?" imageAlt='erik cooking' imageSrc={erikPhoto} side="left" body="I'm based in Chicago. I like taking pictures on my Fuji, and eating all foods. I'm a software engineer specializing in iOS development, and I love tinkering with my computers at home! I play video games mostly on PC, but I promise I am also actively outdoors touching grass and riding my bike around the city."/>
-        <Section title="Photography" imageAlt='butterfly on finger' imageSrc={butterfly} side="right" body="My primary camera is a Fujifilm X-T30iii using an XF 55-200mm lens and the nifty, tiny, and surprisingly powerful Ricoh GRIV. I have examples of my work in the Photography section, with most of my favorite pictures being candid street photography. I am still fairly new to composition and color science, but it is probably my favorite hobby based on the amount of times I pull them out in public."/>
-      </>
+        <h1> Hey, I'm Erik! </h1>
+        <Section title="Who am I?" imageAlt='erik cooking' imageSrc={leadPhoto} side="left" body="I'm based in Chicago. I like taking pictures on my Fuji, and eating all kinds of food! I'm a software engineer specializing in iOS development, and I love tinkering with my computers at home! I love video games, but I am also frequently active lifting weights, walking to nearby restaurants, and riding my bike around the city."/>
+        {/* <Section title="iOS Development" imageAlt='butterfly on finger' imageSrc={flower} side="right" body="My primary camera is a Fujifilm X-T30iii using an XF 55-200mm lens and the nifty, tiny, and surprisingly powerful Ricoh GRIV. I have examples of my work in the Photography section, with most of my favorite pictures being candid street photography. I am still fairly new to composition and color science, but it is probably my favorite hobby based on the amount of times I pull them out in public."/> */}
+        <Section title="Photography" imageAlt='butterfly on finger' imageSrc={butterfly} side="left" body="My primary camera is a Fujifilm X-T30iii using an XF 55-200mm lens and the nifty, tiny, and surprisingly powerful Ricoh GRIV. I have examples of my work in the Photography section, with most of my favorite pictures being candid street photography. I am still fairly new to composition and color science, but it is probably my favorite hobby based on the amount of times I pull them out in public."/>
+    </>
     )
 };
 export default Home
--- src/pages/Photography.tsx
+++ src/pages/Photography.tsx
@@ -1,15 +1,15 @@
-// import erikPhoto from '/erik.png'
+import lighthouse from '../assets/lighthouse.jpeg'
 import '../App.css'
 
 const Photography = () => {
     return (
       <>
-        {/* <div>
-          <img src={erikPhoto} className="erikPhoto" alt="Erik Gamez" />
-        </div> */}
+        <h1> Stay on the lookout for more of my shots </h1>
+        <h2> All taken on a Fujifilm X-T30iii with the XF 1:3.5-4.8 R LM OIS 55-200mm lens </h2>
 
-        <h1> These are some of my favorite captured shots. </h1>
-        <h2> These were taken on my Fujifilm X-T30iii with the XF 1:3.5-4.8 R LM OIS 55-200mm lens </h2>
+        <div>
+          <img src={lighthouse} className="leadPhoto" alt="Erik Gamez" />
+        </div>
       </>
     )
 };
